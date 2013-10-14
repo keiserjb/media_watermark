62e9d93dae80e42899054abddbfd375d517ceac7
Bogdan1988 <bogdan.tur1988@gmail.com>
2013-10-14 17:16:28 +0300

    Added changes according to comments

--- media_watermark.js
+++ media_watermark.js
@@ -3,25 +3,25 @@
  * Media watermark module javascript file.
  * Contains javascript for admin interface of media watermark module.
  */
+
 (function ($) {
 
     Drupal.behaviors.flex2_media_watermark = {
         attach: function (context, settings) {
             jQuery('.hide-select-list').hide();
             jQuery('#edit-watermarks-images .image-hidden').hide();
-            jQuery('#edit-watermarks-images .image-hidden').first().show();
-            jQuery("#edit-no-watermark").click(function(){
-                if($('input[name=no_watermark]').is(':checked')){
-                    jQuery('.form-item-watermarks-names').hide();
-                    jQuery('#edit-watermarks-images .image-hidden').hide();
-                } else {
-                    jQuery('.form-item-watermarks-names').show();
+            jQuery('.form-item-watermarks-names').hide();
+            jQuery("#edit-add-watermark").click(function(){
+                if($('input[name=add_watermark]').is(':checked')){
                     jQuery('#edit-watermarks-images .image-hidden').first().show();
+                    jQuery('.form-item-watermarks-names').show();
                     jQuery('#edit-watermarks-names option').first().attr('selected', true);
                     jQuery('#edit-watermarks-names').siblings('span.custom-select')
                         .find('span span')
                         .text(jQuery('#edit-watermarks-names option').first().text());
-
+                } else {
+                    jQuery('.form-item-watermarks-names').hide();
+                    jQuery('#edit-watermarks-images .image-hidden').hide();
                 }
             });
             jQuery('#edit-watermarks-names').change(function(){
