da6d927e5dd5cf95f15fd3a588f6e0722a22376b
Bogdan1988 <bogdan.tur1988@gmail.com>
2013-11-17 18:48:29 +0200

    Added changes into js file to satisfy coding standards, added changes to load watermark class only when needed.

--- media_watermark.js
+++ media_watermark.js
@@ -6,32 +6,32 @@
  */
 
 (function ($) {
-
-    Drupal.behaviors.flex2_media_watermark = {
-        attach: function (context, settings) {
-            $('.hide-select-list').hide();
-            $('#edit-watermarks-images .image-hidden').hide();
-            $('.form-item-watermarks-names').hide();
-            $("#edit-add-watermark").click(function(){
-                if($('input[name=add_watermark]').is(':checked')){
-                    $('#edit-upload-upload-button, #edit-upload-remove-button').hide();
-                    $('#edit-watermarks-images .image-hidden').first().show();
-                    $('.form-item-watermarks-names').show();
-                    $('#edit-watermarks-names option').first().attr('selected', true);
-                    $('#edit-watermarks-names').siblings('span.custom-select')
-                        .find('span span')
-                        .text($('#edit-watermarks-names option').first().text());
-                } else {
-                    $('#edit-upload-upload-button, #edit-upload-remove-button').show();
-                    $('.form-item-watermarks-names').hide();
-                    $('#edit-watermarks-images .image-hidden').hide();
-                }
-            });
-            $('#edit-watermarks-names').change(function(){
-                var val = $('#edit-watermarks-names :selected').val();
-                $('#edit-watermarks-images .image-hidden').hide();
-                $('#edit-watermarks-images #image-' + val).show();
-            });
+  Drupal.behaviors.flex2_media_watermark = {
+    attach: function (context, settings) {
+      $('.hide-select-list', context).hide();
+      $('#edit-watermarks-images .image-hidden', context).hide();
+      $('.form-item-watermarks-names', context).hide();
+      $("#edit-add-watermark", context).click(function(){
+        if ($('input[name=add_watermark]', context).is(':checked')) {
+          $('#edit-upload-upload-button, #edit-upload-remove-button', context).hide();
+          $('#edit-watermarks-images .image-hidden', context).first().show();
+          $('.form-item-watermarks-names', context).show();
+          $('#edit-watermarks-names option', context).first().attr('selected', true);
+          $('#edit-watermarks-names',context).siblings('span.custom-select')
+            .find('span span')
+            .text($('#edit-watermarks-names option', context).first().text());
         }
-    };
+        else {
+          $('#edit-upload-upload-button, #edit-upload-remove-button', context).show();
+          $('.form-item-watermarks-names', context).hide();
+          $('#edit-watermarks-images .image-hidden', context).hide();
+        }
+      });
+      $('#edit-watermarks-names', context).change( function () {
+        var val = $('#edit-watermarks-names :selected', context).val();
+        $('#edit-watermarks-images .image-hidden', context).hide();
+        $('#edit-watermarks-images #image-' + val, context).show();
+      });
+    }
+  };
 })(jQuery);
